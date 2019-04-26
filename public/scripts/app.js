830b3f4a64ef1a137d7f65deea93c8ca244f4d37
Jhrohman <josephhrohman@gmail.com>
2019-04-26 09:54:54 -0700

    Added js functionality to buttons for transitioning from page to page. Pages still need updated CSS

---
+++ public/scripts/app.js
@@ -0,0 +1,30 @@
+console.log('sanity check')
+
+// Close and Close & Reveal signUpPopUp
+$('#signUpEsc').on('click', (e) => {
+  $('.signUpPopUp').css('display', 'none');
+});
+
+$('#signUpButton').on('click', (e) => {
+  $('.signUpPopUp').css('display', 'none');
+  $('.formInput').css('display', 'flex');
+});
+
+// Close and Close & Reveal successResponse
+$('#formEsc').on('click', (e) => {
+  $('.formInput').css('display', 'none');
+});
+
+$('#submitButton').on('click', (e) => {
+  $('.formInput').css('display', 'none');
+  $('.successResponse').css('display', 'flex');
+});
+
+// Close and Close & Reveal Landing Page
+$('#successEsc').on('click', (e) => {
+  $('.successResponse').css('display', 'none');
+});
+
+$('#backToShopping').on('click', (e) => {
+  $('.successResponse').css('display', 'none');
+});
