9335101ed15b963030cf223972f3811b44d12e1c
Justin Curry <justincurrydev@gmail.com>
2019-04-26 14:11:22 -0700

    dont break anything

--- public/scripts/owner.js
+++ public/scripts/owner.js
@@ -1,10 +1,56 @@
-$.ajax({
-    type: "GET",
-    url: "http://localhost:4000/api/users",
-    data: "data",
-    dataType: "dataType",
-    success: function (json) {
-        $('')
-        
-    }
-});
+$('#users').on('click', function () {
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:4000/api/users",
+        data: "json",
+        success: function (json) {
+            $('#help').append(json.foreach(user => {
+                `<tr>
+                <th scope="row">1</th>
+                <td>${json.data.firstName}</td>
+                <td>${json.data.lastName}</td>
+                <td>${json.data.email}</td>
+                <td>${json.data.date}</td>
+                </tr>`
+            }))
+        }
+    });
+})
+
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:4000/api/users",
+        data: "json",
+        success: function (res) {
+            for(let i = 0; i < res.length; i++) {
+                $('#help').append(`<tr>
+                <th scope="row">1</th>
+                <td>${res[i].firstName}</td>
+                <td>${res[i].lastName}</td>
+                <td>${res[i].email}</td>
+                <td>${res[i].date}</td>
+                </tr>`)
+            }
+            
+        }
+    });
+
+
+
+// res.forEach( (res, i) => {
+//     `<tr>
+//     <th scope="row">1</th>
+//     <td>${res[i].firstName}</td>
+//     <td>${res[i].lastName}</td>
+//     <td>${res[i].email}</td>
+//     <td>${res[i].date}</td>
+//     </tr>`
+// })
+// `<tr>
+// <th scope="row">1</th>
+// <td>${res[i].firstName}</td>
+// <td>${res[i].lastName}</td>
+// <td>${res[i].email}</td>
+// <td>${res[i].date}</td>
+// </tr>
